a541c4a9652b43a37cca0f16bbc0bce00817b92e
Chengguizi Han <13712509+laurelch@users.noreply.github.com>
2021-07-23 11:00:44 -0400

    fix comment in batch file

--- bash/convert.bat
+++ bash/convert.bat
@@ -1,2 +1,2 @@
-：： (Windows) convert all EXR files to PNG with Image Magick
+:: (Windows) convert all EXR files to PNG with Image Magick
 FOR /R %%a IN (*.exr) DO "C:\Program Files\ImageMagick-7.1.0-Q16-HDRI\magick.exe" convert "%%~a" "%%~dpna.png"
